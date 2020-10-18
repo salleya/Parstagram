a8a539fcab44726c5b262fb48bb8f520afbeefa9
Amy Salley <alsalley@icloud.com>
2020-10-17 20:33:31 -0600

    Posts display

--- instagram/AppDelegate.swift
+++ instagram/AppDelegate.swift
@@ -17,8 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         let parseConfig = ParseClientConfiguration {
-                    $0.applicationId = "BCrUQVkk80pCdeImSXoKXL5ZCtyyEZwbN7mAb11f"
-                    $0.clientKey = "rWFPEbTs7UzkaVsIXnQ4qmmr9oWqwXfiiJehtIZu"
+                    $0.applicationId = "KbtQ1RZ3Z5GfTOuBAbiatlU9mVrMY7kHS9YO7W5y"
+                    $0.clientKey = "rlpwxMMUibvUjbK3udNlRla9v0rZLRDdoquGoXRv"
                     $0.server = "https://parseapi.back4app.com"
             }
             Parse.initialize(with: parseConfig)
---
+++ instagram/PostCell.swift
@@ -0,0 +1,28 @@
+//
+//  PostCell.swift
+//  instagram
+//
+//  Created by Salley on 10/17/20.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
